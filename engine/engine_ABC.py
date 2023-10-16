4d637e015aaa593d28e5fc5f04fc4656d2416f9e
Dennis Cherepanov <dennis@cherepanov.org>
2023-10-16 15:36:54 -0500

    Fixed a formatting error in battery_ABC refactored the engine classes by     -adding an abstract base class     -changing all engines to use the engine ABC

--- engine/engine_ABC.py
+++ engine/engine_ABC.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
-class Battery(ABC):
-    '''the abstract class for batteries
+class Engine(ABC):
+    '''the abstract class for engines
     must implement needs_service(self)'''
     @abstractmethod
     def needs_service(self) -> bool:
