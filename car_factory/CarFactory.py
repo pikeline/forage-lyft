c60236efc33fcb5994c5270b53176903cd29123c
Dennis Cherepanov <dennis@cherepanov.org>
2023-10-18 12:18:26 -0500

    made CarFactory's methods static

--- car_factory/CarFactory.py
+++ car_factory/CarFactory.py
@@ -10,30 +10,35 @@ from battery.spindler_battery import SpindlerBattery
 from battery.nubbin_battery import NubbinBattery
 
 class CarFactory():
+    @staticmethod
     def create_calliope(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
         obj = Car()
         obj.engine = CapuletEngine(current_mileage, last_service_mileage)
         obj.battery = SpindlerBattery(current_date, last_service_date)
         return obj
     
+    @staticmethod
     def create_glissade(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
         obj = Car()
         obj.engine = WilloughbyEngine(current_mileage, last_service_mileage)
         obj.battery = SpindlerBattery(current_date, last_service_date)
         return obj
     
+    @staticmethod
     def create_palindrome(current_date: datetime, last_service_date: datetime, warning_light_on : bool) -> Car:
         obj = Car()
         obj.engine = SternmanEngine(warning_light_on)
         obj.battery = SpindlerBattery(current_date, last_service_date)
         return obj
     
+    @staticmethod
     def create_rorschach(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
         obj = Car()
         obj.engine = WilloughbyEngine(current_mileage, last_service_mileage)
         obj.battery = NubbinBattery(current_date, last_service_date)
         return obj
     
+    @staticmethod
     def create_thovex(current_date: datetime, last_service_date: datetime, current_mileage: int, last_service_mileage: int) -> Car:
         obj = Car()
         obj.engine = CapuletEngine(current_mileage, last_service_mileage)
